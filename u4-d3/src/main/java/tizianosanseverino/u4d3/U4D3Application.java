6670951cf904f4684bfc884a72befa862f0ef3cc
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-06-19 14:50:52 +0200

    ADD PROJECT

---
+++ u4-d3/src/main/java/tizianosanseverino/u4d3/U4D3Application.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.u4d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4D3Application.class, args);
+	}
+
+}
---
+++ u4-d3/src/test/java/tizianosanseverino/u4d3/U4D3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.u4d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4D3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
